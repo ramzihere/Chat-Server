c0c5f0f826984e7c980749a269a5c1a822a72a6a
git config --global user.name Your_Name git config --global user.email Your-Email <ramzankhan1272@gmail.com>
2021-02-25 16:49:06 +0500

    Deploying chat application to heroku

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send("You're on top route").status(200);
+  res.send('Server is up and runing').status(200);
 });
 
 module.exports = router;
